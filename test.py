8f6ded07e85783452b8a937179e46888cef62af2
Zander-M <zm800@nyu.edu>
2021-09-02 17:56:21 +0800

    wrap the test function into a library

--- test.py
+++ test.py
@@ -10,83 +10,92 @@ import time
 SET_MODE_DISABLE = b'\xAA\x55\x50\x03\x02\x00'
 
 SET_MODE_1 = b'\xAA\x55\x50\x03\x02\x01'
-
 CHECK_STATUS = b'\xAA\x55\x51\x02\x02'
 
 # Establish Serial Communication
 # put the correct device handle here
-ser = serial.Serial('/dev/ttyUSB1', baudrate=38400)
 
 
-def Set(mode):
-    if mode == 0:
-        data = SET_MODE_DISABLE  # set to wave 1 mode.
-    elif mode == 1:
-        data = SET_MODE_1  # set to wave 1 mode.
-    elif mode == 2:
-        data = CHECK_STATUS  # check status.
-    crc_func = crcmod.predefined.mkCrcFun('crc-8-maxim')
-    pack = data + bytes([crc_func(data)])
-    # use crc8 maxim, according to the doc
-    ser.write(pack)
-    head = ser.read(4)
-    body = ser.read(head[3])
-    receive = head + body
+class Oximeter:
+    def __init__(self, port='/dev/ttyUSB0') -> None:
+        '''
+            Init
+        '''
+        self.ser = serial.Serial(port, baudrate=38400)
+        self.mode = 0
 
-    # check return value to guarantee the correct mode is set
-    while head[2] != data[2]:
-        body = ser.read(head[3])
+    def set(self, mode):
+        '''
+            set mode
+        '''
+        self.mode = mode
+        if mode == 0:
+            data = SET_MODE_DISABLE  # set to wave 1 mode.
+        elif mode == 1:
+            data = SET_MODE_1  # set to wave 1 mode.
+        elif mode == 2:
+            data = CHECK_STATUS  # check status.
+        crc_func = crcmod.predefined.mkCrcFun('crc-8-maxim')
+        pack = data + bytes([crc_func(data)])
+        # use crc8 maxim, according to the doc
+        self.ser.write(pack)
+        head = self.ser.read(4)
+        body = self.ser.read(head[3])
+        receive = head + body
+
+        # check return value to guarantee the correct mode is set
+        while head[2] != data[2]:
+            body = self.ser.read(head[3])
+            receive = head + body
+            for b in receive:
+                print(hex(b), end=" ")
+            print("")
+            self.ser.write(pack)
+            head = self.ser.read(4)
+        body = self.ser.read(head[3])
         receive = head + body
         for b in receive:
             print(hex(b), end=" ")
         print("")
-        ser.write(pack)
-        head = ser.read(4)
-    body = ser.read(head[3])
-    receive = head + body
-    for b in receive:
-        print(hex(b), end=" ")
-    print("")
 
+    def monitor(self):
+        while True:
+            head = self.ser.read(4)
+            body = self.ser.read(head[3])
+            receive = head + body
+            for b in receive:
+                print(hex(b), end=" ")
+            print("")
+            print(self.ser.in_waiting)
 
-def Monitor():
-    while True:
-        head = ser.read(4)
-        body = ser.read(head[3])
-        receive = head + body
-        for b in receive:
-            print(hex(b), end=" ")
-        print("")
-
-
-def Plot():
-    figure = plt.figure()
-    ax = figure.add_subplot(111)
-    plt.ylim((-10, 200))
-    x = np.linspace(0, 2000, 1000)
-    y = np.ones(1000)*64
-    line,  = ax.plot(x, y, 'b-')
-    i = 0
-    figure.canvas.draw()
-    plt.show(block=False)
-    ax.autoscale_view(True, True, True)
-
-    while True:
-        head = ser.read(4)
-        body = ser.read(head[3])
-        receive = head + body
-        for b in receive:
-            print(hex(b), end=" ")
-        if receive[2] == 82:
-            y[i] = receive[5] & 0x7F
-            print('y: ', y[i])
-        else:
-            y[i] = 64
-
-        line.set_ydata(y)
+    def plot(self):
+        figure = plt.figure()
+        ax = figure.add_subplot(111)
+        plt.ylim((-10, 200))
+        x = np.linspace(0, 2000, 1000)
+        y = np.ones(1000)*64
+        line,  = ax.plot(x, y, 'b-')
+        i = 0
         figure.canvas.draw()
-        figure.canvas.flush_events()
-        if i == 999:
-            i = 0
-        else:
-            i = i + 1
+        plt.show(block=False)
+        ax.autoscale_view(True, True, True)
+
+        while True:
+            head = self.ser.read(4)
+            body = self.ser.read(head[3])
+            receive = head + body
+            for b in receive:
+                print(hex(b), end=" ")
+            if receive[2] == 82:
+                y[i] = receive[5] & 0x7F
+                print('y: ', y[i])
+            else:
+                y[i] = 64
+
+            line.set_ydata(y)
+            figure.canvas.draw()
+            figure.canvas.flush_events()
+            if i == 999:
+                i = 0
+            else:
+                i = i + 1
